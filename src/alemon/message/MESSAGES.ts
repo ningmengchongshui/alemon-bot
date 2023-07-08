b6bd1ae369e62a3c4cbafcaab14b32c075496398
柠檬冲水 <916415899@qq.com>
2023-07-08 18:08:57 +0800

    修复

--- src/alemon/message/MESSAGES.ts
+++ src/alemon/message/MESSAGES.ts
@@ -2,6 +2,8 @@ import { Messagetype, EventType, EType, InstructionMatching } from 'alemon'
 /** sdk */
 import { BotEvent, MessageContentType, Client } from 'mys-villa'
 
+const ip = Client.getIP()
+
 /**
  * 消息会话
  * @param event 回调数据
@@ -67,7 +69,7 @@ export async function MESSAGES(event: BotEvent, val: number) {
         if (!uul) {
           return false
         }
-        url = uul
+        url = `http://${ip}:8080:${uul}`
         /** 直接发送图片 */
         return await Client.sendMessageTextUrl(villa_id, room_id, cmd_msg, url).catch(err => {
           console.log(err)
@@ -100,7 +102,7 @@ export async function MESSAGES(event: BotEvent, val: number) {
         if (!uul) {
           return false
         }
-        url = uul
+        url = `http://${ip}:8080:${uul}`
         if (entities.length == 0) {
           return await Client.sendMessageTextUrl(villa_id, room_id, content, url).catch(err => {
             console.log(err)
