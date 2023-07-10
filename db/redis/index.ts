7d363e61595d20492c18f754500a5380a4d97606
柠檬冲水 <10424920+ningmengchongshui@user.noreply.gitee.com>
2023-07-10 15:26:18 +0000

    update db/redis/index.ts.
    Signed-off-by: 柠檬冲水 <10424920+ningmengchongshui@user.noreply.gitee.com>

--- db/redis/index.ts
+++ db/redis/index.ts
@@ -1,8 +1,13 @@
-import { ALRedis } from 'alemon-redis'
-// redis连接示例demo
-ALRedis.set('alemon-plugin', 'alemon-redis OK')
-const val = await ALRedis.get('alemon-plugin').catch(err => {
-  console.log(err)
-  return false
-})
-console.log(val)
+import { createRedis } from "alemon-redis";
+// 不传入配置地址,默认读取config/redis.yaml
+const redisA = createRedis();
+// 传入配置地址
+const redisB = createRedis("config/my/redis.yaml");
+// 传入参数
+const redisC = createRedis({
+  host: "127.0.0.1",
+  port: 6379,
+  password: "",
+  db: 1,
+});
+// 实例可以创建多个,默认使用数据库1
